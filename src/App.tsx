474e2b66c8beed0e31478e5d744f1f1f835532c9
Amanda <agsmarr@udel.edu>
2025-03-28 14:52:57 -0400

    Added about message above home buttons

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the Career Helpi!</h1>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
       <HomeButtons></HomeButtons>
       <Form>
--- src/Components/HomeButtons.tsx
+++ src/Components/HomeButtons.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
 import {Button} from 'react-bootstrap';
 import './HomeButtons.css';
-
 export function HomeButtons() {
     return (<div>
-        <p>Choose a quiz!</p>
+        <h1>Choose a quiz!</h1>
+        <p>Choose between a basic or detailed quiz to discover what type of career is best for you!
+        </p>
         <div className = "Buttons">
         <Button id = "Basic">Basic Questions Quiz</Button>
         <Button id = "Detailed">Detailed Questions Quiz</Button>
