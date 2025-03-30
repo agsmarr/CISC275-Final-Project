189428ac1aee4478e3e391f6a0ac8817d7223385
saieda <saieda@udel.edu>
2025-03-30 17:59:59 -0400

    Descrition for quiz bottons (basic and detailed) are added.

--- src/Components/HomeButtons.tsx
+++ src/Components/HomeButtons.tsx
@@ -6,8 +6,24 @@ export function HomeButtons() {
         <p>Choose between a basic or detailed quiz to discover what type of career is best for you!
         </p>
         <div className = "Buttons">
-        <Button id = "Basic">Basic Questions Quiz</Button>
-        <Button id = "Detailed">Detailed Questions Quiz</Button>
+            <div className="quiz-container">
+                <Button id = "Basic">Basic Questions Quiz</Button>
+                {/* adding Basic Question description */}
+                    <div className="quiz-description">
+                    <p>
+                    Navigate to the 'Basic Questions' Page to answer some basic questions about yourself to determine the career fit for you!
+                    </p>
+        </div>
+        </div> 
+            <div className="quiz-container">
+                <Button id = "Detailed">Detailed Questions Quiz</Button>
+                    {/* adding Detailed Question description */}
+                    <div className="quiz-description">
+                    <p>
+                    Navigate to the 'Detailed Questions' Page to answer some basic questions about yourself to determine the career fit for you!
+                    </p>
+            </div>
+            </div>
         </div>
     </div>)
 }
