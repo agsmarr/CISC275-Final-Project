8d1f2020ceb3d68eede1dec7d339174963e94b9e
Amanda <agsmarr@udel.edu>
2025-04-09 11:09:10 -0400

    Question headers

--- src/Components/BasicQuizPage.tsx
+++ src/Components/BasicQuizPage.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BasicQuizPage.css';
 import { Button } from 'react-bootstrap';
 const BasicQuizPage = () => {
+  const[question, setQuestion] = useState<number>(1);
   const goBackHome = () => {
     window.location.hash = '/';
   };
   return (
     <div>
       <header>
-        <Button variant="secondary" id="home-button" onClick={goBackHome}>
-          Home Page
-        </Button>
+        <div className = "header-buttons">
+          <Button id = "last-button" onClick={() => (question != 1) ? setQuestion(question - 1): setQuestion(question)}>Last Question</Button>
+          <Button variant="secondary" id="home-button" onClick={goBackHome}>
+            Home Page
+          </Button>
+          <Button id = "next-button" onClick={()=> (question != 7) ? setQuestion(question + 1): setQuestion(question)}>Next Question</Button>
+        </div>
       </header>
-      <h1>Basic Questions Quiz</h1>
-      <p>This is the Basic Quiz Page</p>
-      {/* Add your basic quiz content here */}
-      
+      <h1 id = "basic-header">Basic Questions Quiz</h1>
+      <h2>Question {question}</h2>
     </div>
   );
 };
