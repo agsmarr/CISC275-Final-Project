0e91167562d550c068394320e2125514caa2d57b
Amanda <agsmarr@udel.edu>
2025-04-14 11:08:53 -0400

    Changing interface & adding answers

--- src/Components/BasicQuizPage.tsx
+++ src/Components/BasicQuizPage.tsx
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
 import './BasicQuizPage.css';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 const BasicQuizPage = () => {
   const[question, setQuestion] = useState<number>(1);
+  const[answer2, setAnswer2] = useState<string>("");
+  function updateAnswer2(event: React.ChangeEvent<HTMLInputElement>) {
+      setAnswer2(event.target.value);
+  }
   const goBackHome = () => {
     window.location.hash = '/';
   };
@@ -20,16 +24,35 @@ const BasicQuizPage = () => {
         <Button id = "previous-button" onClick={() => (question !== 1) ? setQuestion(question - 1): setQuestion(question)}>Previous</Button>
         <Button id = "next-button" onClick={()=> (question !== 7) ? setQuestion(question + 1): setQuestion(question)}>Next</Button> 
       </div>
-      <div className = "questions">
+      <div className = "question-box">
         {/* Questions go in span tag --> when next question button is clicked, correct question will display */}
-        {(question === 1) ? <span>What subjects or topics do you enjoy the most?</span> : 
-        (question === 2) ? <span>Do you prefer working with people, data, or objects?</span> : 
-        (question === 3) ? <span>Would you prefer to work indoors or outdoors?</span> : 
-        (question === 4) ? <span>Would you like to work alone or in a team?</span> : 
-        (question === 5) ? <span>Do you like leading a team or following instructions?</span> : 
-        (question === 6) ? <span>Do you enjoy routine tasks or trying new things often?</span> : 
-        (question === 7) ? <span>What is more important to you: money, assisting community, or doing what you love?</span> : 
-        <span></span>}
+        <span id = "question1">
+          <div className = "questions">What subjects or topics do you enjoy the most?</div>
+          <div className = "answers1">
+            <Form.Check type = "checkbox" id = "question1-answer1" label = "The Arts"></Form.Check>
+            <Form.Check type = "checkbox" id = "question1-answer2" label = "Humanities"></Form.Check>
+            <Form.Check type = "checkbox" id = "question1-answer3" label = "Social Sciences"></Form.Check>
+            <Form.Check type = "checkbox" id = "question1-answer4" label = "STEM subjects (Science, Technology, Engineering, Math)"></Form.Check>
+            <Form.Check type = "checkbox" id = "question1-answer5" label = "None of the Above"></Form.Check>
+          </div>
+        </span>
+        <span id = "question2">
+          <div className = "questions">Do you prefer working with people, data, or objects?</div>
+          <div className = "answers2">
+            <Form.Check type = "radio" id = "question2-answer1" label = "People" value = "People" onChange={updateAnswer2} checked = {answer2 === "People"}></Form.Check>
+            <Form.Check type = "radio" id = "question2-answer2" label = "Data" value = "Data" onChange={updateAnswer2} checked = {answer2 === "Data"}></Form.Check>
+            <Form.Check type = "radio" id = "question2-answer3" label = "Objects" value = "Objects" onChange={updateAnswer2} checked = {answer2 === "Objects"}></Form.Check>
+          </div>
+        </span>
+        <span>
+          <div id = "quest-3">Would you prefer to work indoors or outdoors?</div>
+        </span>
+        <span>
+          <div id = "quest-4">Would you like to work alone or in a team?</div>
+        </span> 
+        <span>Do you like leading a team or following instructions?</span> : 
+        <span>Do you enjoy routine tasks or trying new things often?</span> : 
+        <span>What is more important to you: money, assisting community, or doing what you love?</span>
       </div>
     </div>
   );
