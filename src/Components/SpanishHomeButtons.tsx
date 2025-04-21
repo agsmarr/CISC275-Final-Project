b2573e0fff6e779c94fb9d1328ae4936c2f97c98
Amanda <agsmarr@udel.edu>
2025-04-20 21:48:01 -0400

    Worked on spanish language implementation

--- src/Components/SpanishHomeButtons.tsx
+++ src/Components/SpanishHomeButtons.tsx
@@ -1,37 +1,36 @@
 import {Button} from 'react-bootstrap';
 import './HomeButtons.css';
-export function HomeButtons() {
+export function SpanHomeButtons() {
     const goToBasicQuiz = () => {
-        window.location.hash = '/span-basic-quiz';
+        window.location.hash = '/basic-quiz';
       };
     
     const goToDetailedQuiz = () => {
-        window.location.hash = '/span-detailed-quiz';
+        window.location.hash = '/detailed-quiz';
       };
         return (<div>
-            <h1 id = "bottom-h1">Choose a quiz!</h1>
-            <p id = "bottom-p">Choose between a basic or detailed quiz to discover what type of career is best for you!
+            <h1 id = "bottom-h1">¡Elige un test!</h1>
+            <p id = "bottom-p">¡Elige entre un test básico o detallado para descubrir qué tipo de carrera es mejor para ti!
             </p>
             <div className = "Buttons">
                 <div className="quiz-container">
-                    {/*<Link to="/basic-quiz">*/}
-                    <Button id="Basic" onClick={goToBasicQuiz}>Basic Questions Quiz</Button>
-                    
+                    {/*<Link to="/span-basic-quiz">*/}
+                    <Button id="Basic" onClick={goToBasicQuiz}>Cuestionario de Preguntas Básicas</Button>
                     {/* adding Basic Question description */}
                         <div className="quiz-description">
                         <p>
-                        Navigate to the 'Basic Questions' Page to answer some basic questions about yourself to determine the career fit for you!
+                        ¡Vaya a la página de 'Preguntas básicas' para responder algunas preguntas básicas sobre usted y determinar qué carrera es la adecuada para usted!
                         </p>
             </div>
             </div> 
                 <div className="quiz-container">
-                    {/*<Link to="/detailed-quiz">*/}
-                    <Button id="Detailed" onClick={goToDetailedQuiz}>Detailed Questions Quiz</Button>
+                    {/*<Link to="/span-detailed-quiz">*/}
+                    <Button id="Detailed" onClick={goToDetailedQuiz}>Cuestionario de Preguntas Detalladas</Button>
                     {/*</Link>*/}
                         {/* adding Detailed Question description */}
                         <div className="quiz-description">
                         <p>
-                        Navigate to the 'Detailed Questions' Page to answer some basic questions about yourself to determine the career fit for you!
+                        ¡Vaya a la página 'Preguntas detalladas' para responder algunas preguntas básicas sobre usted y determinar la carrera profesional adecuada para usted!
                         </p>
                 </div>
                 </div>
