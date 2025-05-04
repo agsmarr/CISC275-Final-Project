6e976df18c630eb93747b693968f1ae6ea66cc7f
Amanda <agsmarr@udel.edu>
2025-05-04 14:35:45 -0400

    Added CSS to intro boxes

--- src/Components/AboutUsPage.tsx
+++ src/Components/AboutUsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import './AboutUsPage.css';
+import image3 from './ComponentImages/Amanda.jpg';
 function AboutUs() {
     const goBackHome = () => {
         window.location.hash = '/';
@@ -24,12 +25,17 @@ function AboutUs() {
                 <a href = "mailto:yaqingj@udel.edu">yaqingj@udel.edu</a>
             </div>
         </div>
-        <div className = "picture-box">
-            <p>Amanda Smarr</p>
-            <a href = "mailto:agsmarr@udel.edu">agsmarr@udel.edu</a>
+        <div className = "intro-border">
+            <div id = "Amanda-intro">
+                <div className = "picture-box">
+                    <img src = {image3} alt = ""></img>
+                    <p>Amanda Smarr</p>
+                    <a href = "mailto:agsmarr@udel.edu">agsmarr@udel.edu</a>
+                </div>
+                <p className = "intros">Hi! I'm Amanda. I'm a sophomore at UD. I am a CS major with a cybersecurity concentration, and I currently work as an organizer for the HenHacks team.</p>
+            </div>
         </div>
-        <p className = "intros">Hi! I'm Amanda. I'm a sophomore at UD. I am a CS major with a cybersecurity concentration, and I currently work as an organizer for the HenHacks team.</p>
-        </div>
+    </div>
     </div>)
 }
 export default AboutUs
