782f569433a77fa961dbc32eb80ed44483341879
Amanda <agsmarr@udel.edu>
2025-05-05 20:50:15 -0400

    Finished loading pop-up

--- src/Components/BasicQuizPage.tsx
+++ src/Components/BasicQuizPage.tsx
@@ -100,79 +100,82 @@ const BasicQuizPage = () => {
   
   return (
     <div>
-      <header>
-          <Button variant="secondary" id="home-button" onClick={goBackHome}>
-            Home Page
+        {loading ? <LoadingScreen></LoadingScreen> : 
+        <div>
+        <header>
+            <Button variant="secondary" id="home-button" onClick={goBackHome}>
+              Home Page
+            </Button>
+        </header>
+        <h1 id="basic-header">Basic Questions Quiz</h1>
+        <div className="sticky-progress-bar mt-4">
+          <ProgressBar now={progress} variant="info" label={`${Math.round(progress)}%`} />
+        </div>
+        <div className="question-box">
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 1</h2>
+            <div className="questions">What subjects or topics do you enjoy the most?</div>
+            <div className="answers">
+              <Form.Check type="checkbox" id="question1-answer1" label="The Arts" value="The Arts" onChange={updateAnswer1} checked={answer1.includes("The Arts")} />
+              <Form.Check type="checkbox" id="question1-answer2" label="Humanities" value="Humanities" onChange={updateAnswer1} checked={answer1.includes("Humanities")} />
+              <Form.Check type="checkbox" id="question1-answer3" label="Social Sciences" value="Social Sciences" onChange={updateAnswer1} checked={answer1.includes("Social Sciences")} />
+              <Form.Check type="checkbox" id="question1-answer4" label="STEM subjects (Science, Technology, Engineering, Math)" value="STEM subjects (Science, Technology, Engineering, Math)" onChange={updateAnswer1} checked={answer1.includes("STEM subjects (Science, Technology, Engineering, Math)")} />
+              <Form.Check type="checkbox" id="question1-answer5" label="None of the Above" value="None of the Above" onChange={updateAnswer1} checked={answer1.includes("None of the Above")} />
+            </div>
+          </div>
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 2</h2>
+            <div className="questions">Do you prefer working with people, data, or objects?</div>
+            <div className="answers">
+              <Form.Check type="radio" id="question2-answer1" label="People" value="People" onChange={updateAnswer2} checked={answer2 === "People"} />
+              <Form.Check type="radio" id="question2-answer2" label="Data" value="Data" onChange={updateAnswer2} checked={answer2 === "Data"} />
+              <Form.Check type="radio" id="question2-answer3" label="Objects" value="Objects" onChange={updateAnswer2} checked={answer2 === "Objects"} />
+            </div>
+          </div>
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 3</h2>
+            <div className="questions">Would you prefer to work indoors or outdoors?</div>
+            <Form.Check type="radio" id="question3-answer1" label="Indoors" value="Indoors" onChange={updateAnswer3} checked={answer3 === "Indoors"} />
+            <Form.Check type="radio" id="question3-answer2" label="Outdoors" value="Outdoors" onChange={updateAnswer3} checked={answer3 === "Outdoors"} />
+          </div>
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 4</h2>
+            <div className="questions">Would you like to work alone or in a team?</div>
+            <Form.Check type="radio" id="question4-answer1" label="Alone" value="Alone" onChange={updateAnswer4} checked={answer4 === "Alone"} />
+            <Form.Check type="radio" id="question4-answer2" label="In a Team" value="In a Team" onChange={updateAnswer4} checked={answer4 === "In a Team"} />
+          </div> 
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 5</h2>
+            <div className="questions">Do you like leading a team or following instructions?</div>
+            <Form.Check type="radio" id="question5-answer1" label="Leading a Team" value="Leading a Team" onChange={updateAnswer5} checked={answer5 === "Leading a Team"} />
+            <Form.Check type="radio" id="question5-answer2" label="Following Instructions" value="Following Instructions" onChange={updateAnswer5} checked={answer5 === "Following Instructions"} />
+          </div> 
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 6</h2>
+            <div className="questions">Do you enjoy routine tasks or trying new things often?</div>
+            <Form.Check type="radio" id="question6-answer1" label="Routine Tasks" value="Routine Tasks" onChange={updateAnswer6} checked={answer6 === "Routine Tasks"} />
+            <Form.Check type="radio" id="question6-answer2" label="Trying New Things" value="Trying New Things" onChange={updateAnswer6} checked={answer6 === "Trying New Things"} />
+          </div>
+          <div className="question-boxes">
+            <h2 className="question-headers">Question 7</h2>
+            <div className="questions">What is more important to you: money, assisting community, or doing what you love?</div>
+            <Form.Check type="radio" id="question7-answer1" label="Money" value="Money" onChange={updateAnswer7} checked={answer7 === "Money"} />
+            <Form.Check type="radio" id="question7-answer2" label="Assisting Community" value="Assisting Community" onChange={updateAnswer7} checked={answer7 === "Assisting Community"} />
+            <Form.Check type="radio" id="question7-answer3" label="Doing What You Love" value="Doing What You Love" onChange={updateAnswer7} checked={answer7 === "Doing What You Love"} />
+          </div>
+          <Button
+            id="Submit-Button"
+            onClick={handleSubmit}
+            disabled={!isAllAnswered || loading}
+            style={{
+              backgroundColor: isAllAnswered ? 'purple' : 'grey',
+              cursor: isAllAnswered ? 'pointer' : 'not-allowed',
+            }}
+          >
+            Get Results!
           </Button>
-      </header>
-      <h1 id="basic-header">Basic Questions Quiz</h1>
-      <div className="sticky-progress-bar mt-4">
-        <ProgressBar now={progress} variant="info" label={`${Math.round(progress)}%`} />
-      </div>
-      <div className="question-box">
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 1</h2>
-          <div className="questions">What subjects or topics do you enjoy the most?</div>
-          <div className="answers">
-            <Form.Check type="checkbox" id="question1-answer1" label="The Arts" value="The Arts" onChange={updateAnswer1} checked={answer1.includes("The Arts")} />
-            <Form.Check type="checkbox" id="question1-answer2" label="Humanities" value="Humanities" onChange={updateAnswer1} checked={answer1.includes("Humanities")} />
-            <Form.Check type="checkbox" id="question1-answer3" label="Social Sciences" value="Social Sciences" onChange={updateAnswer1} checked={answer1.includes("Social Sciences")} />
-            <Form.Check type="checkbox" id="question1-answer4" label="STEM subjects (Science, Technology, Engineering, Math)" value="STEM subjects (Science, Technology, Engineering, Math)" onChange={updateAnswer1} checked={answer1.includes("STEM subjects (Science, Technology, Engineering, Math)")} />
-            <Form.Check type="checkbox" id="question1-answer5" label="None of the Above" value="None of the Above" onChange={updateAnswer1} checked={answer1.includes("None of the Above")} />
-          </div>
-        </div>
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 2</h2>
-          <div className="questions">Do you prefer working with people, data, or objects?</div>
-          <div className="answers">
-            <Form.Check type="radio" id="question2-answer1" label="People" value="People" onChange={updateAnswer2} checked={answer2 === "People"} />
-            <Form.Check type="radio" id="question2-answer2" label="Data" value="Data" onChange={updateAnswer2} checked={answer2 === "Data"} />
-            <Form.Check type="radio" id="question2-answer3" label="Objects" value="Objects" onChange={updateAnswer2} checked={answer2 === "Objects"} />
-          </div>
-        </div>
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 3</h2>
-          <div className="questions">Would you prefer to work indoors or outdoors?</div>
-          <Form.Check type="radio" id="question3-answer1" label="Indoors" value="Indoors" onChange={updateAnswer3} checked={answer3 === "Indoors"} />
-          <Form.Check type="radio" id="question3-answer2" label="Outdoors" value="Outdoors" onChange={updateAnswer3} checked={answer3 === "Outdoors"} />
-        </div>
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 4</h2>
-          <div className="questions">Would you like to work alone or in a team?</div>
-          <Form.Check type="radio" id="question4-answer1" label="Alone" value="Alone" onChange={updateAnswer4} checked={answer4 === "Alone"} />
-          <Form.Check type="radio" id="question4-answer2" label="In a Team" value="In a Team" onChange={updateAnswer4} checked={answer4 === "In a Team"} />
-        </div> 
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 5</h2>
-          <div className="questions">Do you like leading a team or following instructions?</div>
-          <Form.Check type="radio" id="question5-answer1" label="Leading a Team" value="Leading a Team" onChange={updateAnswer5} checked={answer5 === "Leading a Team"} />
-          <Form.Check type="radio" id="question5-answer2" label="Following Instructions" value="Following Instructions" onChange={updateAnswer5} checked={answer5 === "Following Instructions"} />
-        </div> 
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 6</h2>
-          <div className="questions">Do you enjoy routine tasks or trying new things often?</div>
-          <Form.Check type="radio" id="question6-answer1" label="Routine Tasks" value="Routine Tasks" onChange={updateAnswer6} checked={answer6 === "Routine Tasks"} />
-          <Form.Check type="radio" id="question6-answer2" label="Trying New Things" value="Trying New Things" onChange={updateAnswer6} checked={answer6 === "Trying New Things"} />
-        </div>
-        <div className="question-boxes">
-          <h2 className="question-headers">Question 7</h2>
-          <div className="questions">What is more important to you: money, assisting community, or doing what you love?</div>
-          <Form.Check type="radio" id="question7-answer1" label="Money" value="Money" onChange={updateAnswer7} checked={answer7 === "Money"} />
-          <Form.Check type="radio" id="question7-answer2" label="Assisting Community" value="Assisting Community" onChange={updateAnswer7} checked={answer7 === "Assisting Community"} />
-          <Form.Check type="radio" id="question7-answer3" label="Doing What You Love" value="Doing What You Love" onChange={updateAnswer7} checked={answer7 === "Doing What You Love"} />
-        </div>
-        <Button
-          id="Submit-Button"
-          onClick={handleSubmit}
-          disabled={!isAllAnswered || loading}
-          style={{
-            backgroundColor: isAllAnswered ? 'purple' : 'grey',
-            cursor: isAllAnswered ? 'pointer' : 'not-allowed',
-          }}
-        >
-          Get Results!
-        </Button>
-        {loading && <div><LoadingScreen></LoadingScreen></div>}
+         </div>
+         </div>}
         {showReport && (
           <div className="report-section" style={{ 
             marginTop: '20px',
@@ -199,7 +202,6 @@ const BasicQuizPage = () => {
           </div>
         )}
       </div>
-    </div>
   );
 };
 
